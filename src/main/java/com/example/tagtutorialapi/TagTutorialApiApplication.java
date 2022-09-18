cc4aa05444529f9526222f0ef619ce249144f740
felixmaina <karani.maina2010@gmail.com>
2022-09-18 23:20:06 +0300

    first commit

---
+++ src/main/java/com/example/tagtutorialapi/TagTutorialApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tagtutorialapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagTutorialApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagTutorialApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tagtutorialapi/TagTutorialApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tagtutorialapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagTutorialApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
