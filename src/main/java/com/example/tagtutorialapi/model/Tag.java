ba22aca26fba483e7cb0eef8e5810d0cbc258090
felixmaina <karani.maina2010@gmail.com>
2022-09-18 23:28:09 +0300

    add a setter

--- src/main/java/com/example/tagtutorialapi/model/Tag.java
+++ src/main/java/com/example/tagtutorialapi/model/Tag.java
@@ -1,15 +1,18 @@
 package com.example.tagtutorialapi.model;
 
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 
 @Entity
 @Getter
+@Setter
 
 public class Tag {
     private Long id;
     private String name;
+    privat
 
 
 }
