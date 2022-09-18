15212b4da28a1470ab1dd15149911d0f16247462
felixmaina <karani.maina2010@gmail.com>
2022-09-18 23:27:12 +0300

    add a Getter

--- src/main/java/com/example/tagtutorialapi/model/Tag.java
+++ src/main/java/com/example/tagtutorialapi/model/Tag.java
@@ -1,12 +1,15 @@
 package com.example.tagtutorialapi.model;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 
 @Entity
+@Getter
 
 public class Tag {
     private Long id;
     private String name;
-    p
+
 
 }
