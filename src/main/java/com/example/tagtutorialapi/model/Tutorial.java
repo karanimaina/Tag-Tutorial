242f09c16b66a3a4fa167db1a3039c13e858fb14
felixmaina <karani.maina2010@gmail.com>
2022-09-18 23:35:12 +0300

    add an all args constructors

--- src/main/java/com/example/tagtutorialapi/model/Tutorial.java
+++ src/main/java/com/example/tagtutorialapi/model/Tutorial.java
@@ -1,10 +1,12 @@
 package com.example.tagtutorialapi.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class Tutorial {
 private Long id;
 private String title;
