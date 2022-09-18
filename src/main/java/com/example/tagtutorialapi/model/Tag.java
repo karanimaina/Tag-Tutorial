0eab018c7475439b78dad0b50faa6dbd20554c6b
felixmaina <karani.maina2010@gmail.com>
2022-09-18 23:54:30 +0300

    add a Manyto Many tag mapped to tutarial tag attribute

--- src/main/java/com/example/tagtutorialapi/model/Tag.java
+++ src/main/java/com/example/tagtutorialapi/model/Tag.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.security.CodeSigner;
 import java.util.HashSet;
 import java.util.Set;
@@ -23,7 +20,7 @@ public class Tag {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
-
+@ManyToMany(fetch = FetchType.LAZY,mappedBy = "tags")
     private Set<Tutorial> tutorials= new HashSet<>();
 
 
