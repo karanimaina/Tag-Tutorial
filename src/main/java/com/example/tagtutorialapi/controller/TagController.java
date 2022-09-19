cfb9294903868a2fc39e8156a8742d52aad04747
felixmaina <karani.maina2010@gmail.com>
2022-09-19 13:54:08 +0300

    refactor the tagController'

--- src/main/java/com/example/tagtutorialapi/controller/TagController.java
+++ src/main/java/com/example/tagtutorialapi/controller/TagController.java
@@ -17,7 +17,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
-public class TagTutorialController {
+public class TagController {
     private final TagRepository tagRepository;
     private final TutorialRepository tutorialRepository;
 
