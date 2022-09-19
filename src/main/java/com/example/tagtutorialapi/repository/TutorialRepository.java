79c9acc1bbce247f04cc164145e58980b10ba65f
felixmaina <karani.maina2010@gmail.com>
2022-09-19 11:12:45 +0300

    getTuttorial by published id

---
+++ src/main/java/com/example/tagtutorialapi/repository/TutorialRepository.java
@@ -0,0 +1,12 @@
+package com.example.tagtutorialapi.repository;
+
+import com.example.tagtutorialapi.model.Tutorial;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial,Long> {
+    List<Tutorial> findByPublished(boolean Published);
+
+}
