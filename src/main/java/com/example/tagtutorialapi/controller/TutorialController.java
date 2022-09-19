3ddbdfdc39371de22ef08fe7435a1e6458350ed5
felixmaina <karani.maina2010@gmail.com>
2022-09-19 13:56:46 +0300

    add a tutorial repository

--- src/main/java/com/example/tagtutorialapi/controller/TutorialController.java
+++ src/main/java/com/example/tagtutorialapi/controller/TutorialController.java
@@ -1,9 +1,12 @@
 package com.example.tagtutorialapi.controller;
 
+import com.example.tagtutorialapi.repository.TutorialRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class TutorialController {
+    private final TutorialRepository tutorialRepository;
+
 }
