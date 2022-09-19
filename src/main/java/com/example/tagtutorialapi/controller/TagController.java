0c0c9118a96dbd75413b953bd7e90791656997b7
felixmaina <karani.maina2010@gmail.com>
2022-09-19 11:23:49 +0300

    add tag Service

--- src/main/java/com/example/tagtutorialapi/controller/TagController.java
+++ src/main/java/com/example/tagtutorialapi/controller/TagController.java
@@ -1,4 +1,10 @@
 package com.example.tagtutorialapi.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class TagController {
+
 }
---
+++ src/main/java/com/example/tagtutorialapi/service/TagService.java
@@ -0,0 +1,4 @@
+package com.example.tagtutorialapi.service;
+
+public class TagService {
+}
