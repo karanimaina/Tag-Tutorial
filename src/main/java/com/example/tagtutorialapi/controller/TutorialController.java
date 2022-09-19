311c5592d5436777dd332c355fd85246b91b4be9
felixmaina <karani.maina2010@gmail.com>
2022-09-19 14:53:02 +0300

    getTutorial list published by id

--- src/main/java/com/example/tagtutorialapi/controller/TutorialController.java
+++ src/main/java/com/example/tagtutorialapi/controller/TutorialController.java
@@ -79,6 +79,7 @@ public class TutorialController {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
 
+
         return new ResponseEntity<>(tutorials, HttpStatus.OK);
     }
 }
