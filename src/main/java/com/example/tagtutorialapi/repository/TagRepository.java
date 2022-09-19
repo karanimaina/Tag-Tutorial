80a34c0bec0d4e351a4d912972e85ca179f3062e
felixmaina <karani.maina2010@gmail.com>
2022-09-19 11:07:08 +0300

    add tag by tutorialId

--- src/main/java/com/example/tagtutorialapi/repository/TagRepository.java
+++ src/main/java/com/example/tagtutorialapi/repository/TagRepository.java
@@ -3,5 +3,8 @@ package com.example.tagtutorialapi.repository;
 import com.example.tagtutorialapi.model.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TagRepository extends JpaRepository<Tag,Long> {
+    List<Tag>findTagByTutorialsId(Long tutorialId);
 }
