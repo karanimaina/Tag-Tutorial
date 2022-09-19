822dca91a1d081cf29f0a5f6fd51136433726a65
felixmaina <karani.maina2010@gmail.com>
2022-09-19 15:36:53 +0300

    modify the exception handleer

--- src/main/java/com/example/tagtutorialapi/exceptions/ControllerException.java
+++ src/main/java/com/example/tagtutorialapi/exceptions/ControllerException.java
@@ -9,5 +9,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class ControllerException {
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(value = HttpStatus.NOT_FOUND)
-    public ResponseEntity<UniversalResponse> ResourceNotFoundException
+    public ResponseEntity<UniversalResponse> ResourceNotFoundException(ResourceNotFoundException e){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(UniversalResponse.builder().status(404) .message(e.getMessage()).build());
+    }
 }
